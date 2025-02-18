8bb5b45d472d23bd81ed88ab754438d295238daa
Gabriele Zurla <gabriele.zurla@gpi.it>
2025-02-18 10:51:47 +0100

    fix per cie/spid

--- ios/Plugin/WKWebViewController.swift
+++ ios/Plugin/WKWebViewController.swift
@@ -43,7 +43,7 @@ extension Dictionary {
     }
 }
 
-open class WKWebViewController: UIViewController, WKScriptMessageHandler {
+open class WKWebViewController: UIViewController, WKScriptMessageHandler, UIScrollViewDelegate {
 
     public init() {
         super.init(nibName: nil, bundle: nil)
@@ -300,6 +300,7 @@ open class WKWebViewController: UIViewController, WKScriptMessageHandler {
 
         self.extendedLayoutIncludesOpaqueBars = true
         self.edgesForExtendedLayout = [.bottom]
+        let DEV_EXTRA_KEY: String = "developerExtrasEnabled"
 
         let webConfiguration = WKWebViewConfiguration()
         let userContentController = WKUserContentController()
@@ -309,6 +310,7 @@ open class WKWebViewController: UIViewController, WKScriptMessageHandler {
         userContentController.add(self, name: "close")
         webConfiguration.allowsInlineMediaPlayback = true
         webConfiguration.userContentController = userContentController
+        webConfiguration.preferences.setValue(true, forKey: DEV_EXTRA_KEY)
         let webView = WKWebView(frame: .zero, configuration: webConfiguration)
 
         if webView.responds(to: Selector(("setInspectable:"))) {
@@ -328,11 +330,25 @@ open class WKWebViewController: UIViewController, WKScriptMessageHandler {
         }
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.url), options: .new, context: nil)
         //        NotificationCenter.default.addObserver(self, selector: #selector(restateViewHeight), name: UIDevice.orientationDidChangeNotification, object: nil)
+        if #available(iOS 13.0, *) {
+            let CUSTOM_USER_AGENT: String = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1"
+            webView.customUserAgent = CUSTOM_USER_AGENT
+        }
 
-        self.view = webView
+        webView.backgroundColor = .white
+        webView.navigationDelegate = self;
+
+        webView.scrollView.delegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addSubview(webView)
+
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[webView]-0-|", metrics: nil, views: ["webView": webView]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[webView]-0-|", metrics: nil, views: ["webView": webView]))
+
         self.webView = webView
 
-        self.webView?.customUserAgent = self.customUserAgent ?? self.userAgent ?? self.originalUserAgent
+        //self.webView?.customUserAgent = self.customUserAgent ?? self.userAgent ?? self.originalUserAgent
 
         self.navigationItem.title = self.navigationItem.title ?? self.source?.absoluteString
 
